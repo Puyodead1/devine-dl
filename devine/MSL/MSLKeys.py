ad1461f7b69a7482da553c46bbe4a50d9103a496
Puyodead1 <puyodead@protonmail.com>
2025-08-24 15:11:01 -0400

    add MSL

---
+++ devine/MSL/MSLKeys.py
@@ -0,0 +1,12 @@
+from devine.MSL.MSLObject import MSLObject
+
+
+class MSLKeys(MSLObject):
+    def __init__(
+        self, encryption=None, sign=None, rsa=None, mastertoken=None, cdm_session=None
+    ):
+        self.encryption = encryption
+        self.sign = sign
+        self.rsa = rsa
+        self.mastertoken = mastertoken
+        self.cdm_session = cdm_session
